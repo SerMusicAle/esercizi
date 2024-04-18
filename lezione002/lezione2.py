dc0462baaa6eefa77176676039f05cb33d67628b
SerMusicAle <a.sereni@live.it>
2024-04-18 12:24:29 +0200

    risolto primo esercizio

---
+++ lezione002/lezione2.py
@@ -0,0 +1,19 @@
+#Alessandro Sereniù
+#2024 04 18
+
+#ex. prova
+print ("Hello Word")
+
+"""
+ex.2-3. Personal Message: Use a variable to represent a person’s name, and print a message to that person. 
+Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+"""
+#variabile nome
+name: str = "Alessandro"
+
+#variabile mesaggio
+messaggio: str= f"ciao" {name}, "proviamo a stampare"
+
+print (message)
+
+"""
