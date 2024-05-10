fb6aa9410fb8ae77254648da23d27a0f611c5ead
Nio4124 <antoniozottola41204@gmail.com>
2024-05-10 19:25:53 +0200

    aggiornamento tracce

---
+++ lezione010 test/Es_05.py
@@ -0,0 +1,8 @@
+"""
+    Scrivi una funzione che accetti un dizionario di prodotti con i prezzi e restituisca un nuovo dizionario con solo i prodotti che hanno un prezzo superiore a 20, scontati del 10%.
+"""
+
+
+def filtra_e_mappa(prodotti: dict[str:float]) -> list[str:float]:
+    # cancella pass e scrivi il tuo codice
+    pass
