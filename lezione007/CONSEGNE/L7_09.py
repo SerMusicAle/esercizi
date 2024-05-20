3afdb4d49228c5ad168a0431c07c022f10118ca5
SerMusicAle <a.sereni@live.it>
2024-05-20 19:19:44 +0200

    update

--- lezione007/CONSEGNE/L7_09.py
+++ lezione007/CONSEGNE/L7_09.py
@@ -1,4 +1,43 @@
 """
 CONSEGNA
-9. Scrivi una funzione che converta una lista di tuple (chiave, valore) in un dizionario. Se la chiave è già presente, aggiungi il valore alla lista di valori già associata alla chiave.
+9. Scrivi una funzione che converta una lista di tuple (chiave, valore) in un dizionario. 
+Se la chiave è già presente, aggiungi il valore alla lista di valori già associata alla chiave.
 """
+
+def convertitore (listadati):
+    
+    #INIT
+    abbinamenti:list  = {}
+    #BODY
+    for chiave, valore in listadati:
+        if chiave in abbinamenti:
+            abbinamenti[chiave].append(valore)
+        else:
+            abbinamenti[chiave] = [valore]
+    print (f"gli elementi del dizionario sono")
+    for chiave, valore in abbinamenti.items():
+        print (f"a {chiave} corrisponde {valore}")
+
+
+#INPUT
+listadati = [
+    (1, 'a'), 
+    (2, 'b'), 
+    (2, 'c'), 
+    (3, 'd'), 
+    (3, 'e'), 
+    (3, 'f'), 
+    (4, 'g'), 
+    (4, 'h'), 
+    (4, 'i'), 
+    (4, 'j'), 
+    (5, 'k'), 
+    (5, 'l'), 
+    (5, 'm'), 
+    (5, 'n'), 
+    (5, 'o')
+]
+
+
+#CALL
+convertitore (listadati)
