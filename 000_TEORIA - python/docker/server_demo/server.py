ae6cda6d48d3b37aefc1b43df5d0102b53b2d591
Nio4124 <antoniozottola41204@gmail.com>
2024-09-13 17:32:10 +0200

    update

--- 000_TEORIA - python/docker/server_demo/server.py
+++ 000_TEORIA - python/docker/server_demo/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 api = Flask(__name__, 
             static_url_path='',
@@ -6,12 +6,36 @@ api = Flask(__name__,
             template_folder='./templates'
             )
 
+utenti = [
+    ['mario', 'bros', 'M', '0'],
+    ['luigi', 'bros', 'M', '0'],
+    ['daisy', 'princess', 'F', '0'],
+    ['peach', 'slave', 'F', '0']
+]
+
 @api.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@api.route('/pages/milano.html', methods=['GET'])
-def index2():
-    return render_template('/pages/milano.html')
+@api.route('/regok', methods=['GET'])
+def regok():
+    return render_template('/regok.html')
+
+@api.route('/regko', methods=['GET'])
+def regko():
+    return render_template('/regko.html')
+
+
+@api.route('/registrati', methods=['GET'])
+def registrati():
+    nome = request.args.get("name")
+    print("Nome inserito:" + nome)
+
+    password = request.args.get("cognome")
+
+    return render_template('/index.html')
+
 
 api.run(host="0.0.0.0",port=8085)
+
+
