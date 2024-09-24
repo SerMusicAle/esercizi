640245624d7525f577374b0fd2d84024fe1881c3
SerMusicAle <a.sereni@live.it>
2024-09-24 14:57:28 +0200

    update

--- 003_DB_PROGETTAZIONE/Accademia/Accademia5_query.sql
+++ 003_DB_PROGETTAZIONE/Accademia/Accademia5_query.sql
@@ -3,16 +3,20 @@
 
 --1. Quali sono il nome, la data di inizio e la data di fine 
     -- dei WP del progetto di nome ‘Pegasus’?
-SELECT nome, inizio, fine
+SELECT wp.nome, wp.inizio, wp.fine
 FROM WP
-WHERE nome = Pegasus;
+JOIN progetto P
+    ON wp.progetto = p.id
+WHERE p.nome = 'Pegasus';
 
 --2. Quali sono il nome, il cognome e la posizione degli strutturati che hanno 
     --almeno una attività nel progetto ‘Pegasus’, ordinati per cognome decrescente?
 SELECT PA.nome, PA.cognome, PA.posizione
 FROM persona PA
-JOIN attivitaProgetto AP ON PA.id = AP.persona
-JOIN progetto PR ON AP.progetto = PR.id
+JOIN attivitaProgetto AP 
+    ON PA.id = AP.persona
+JOIN progetto PR 
+    ON AP.progetto = PR.id
 WHERE PR.nome = 'Pegasus'
 ORDER BY PA.cognome DESC;
 
@@ -20,35 +24,60 @@ ORDER BY PA.cognome DESC;
     --che hanno più di una attività nel progetto ‘Pegasus’?
 SELECT P.nome, P.cognome, P.posizione
 FROM persona P
-JOIN attivitaProgetto AP ON P.id = AP.persona
-JOIN progetto PR ON AP.progetto = PR.id
+JOIN attivitaProgetto AP 
+    ON P.id = AP.persona
+JOIN progetto PR 
+    ON AP.progetto = PR.id
 WHERE PR.nome = 'Pegasus'
 GROUP BY P.nome, P.cognome, P.posizione
 HAVING COUNT(AP.id) > 1;
+--VERSIONE 2
+--SELECT DISTINCT s.nome, s.conome, s.posizione
+--FROM progetto P
+--  JOIN attivitaprogetto A1
+--      ON P.id = AL.progetto
+--  JOIN attivitaprogetto A2
+--      ON P.id = A2.progetto
+--  JOIN persona S
+--      ON S.id = A1.persona
+--      AND A1.persona = A2.persona
+--WHERE
+--  AND A1.id <>A2.id
+--  AND P.nome = 'Pegasus';
 
 --4. Quali sono il nome, il cognome e la posizione dei Professori Ordinari 
     --che hanno fatto almeno una assenza per malattia?
-SELECT P.nome, P.cognome, P.posizione
+SELECT DISTINCT P.nome, P.cognome, P.posizione
 FROM persona P
-JOIN assenza A ON P.id = A.persona
+JOIN assenza A 
+    ON P.id = A.persona
 WHERE P.posizione = 'Professore Ordinario' 
     AND A.tipo = 'Malattia';
 
 --5. Quali sono il nome, il cognome e la posizione dei Professori Ordinari 
     --che hanno fatto più di una assenza per malattia?
-SELECT P.nome, P.cognome, P.posizione
+SELECT DISTINCT P.nome, P.cognome, P.posizione
 FROM persona P
-JOIN assenza A ON P.id = A.persona
+JOIN assenza A 
+    ON P.id = A.persona
 WHERE P.posizione = 'Professore Ordinario'
     AND A.tipo = 'Malattia'
 GROUP BY P.nome, P.cognome, P.posizione
 HAVING COUNT(A.id) > 1;
+--VERSIONE 2
+--SELECT DISTINCT nome, cognome, posizione
+--FROM assenza A1, assenza A2, persona S
+--WHERE A1.id <> A2.id
+--  AND A1.persona = A2.persona
+--  AND A1.persona = S.id
+--  AND S.posizione = 'Professore Ordinario'
 
 --6. Quali sono il nome, il cognome e la posizione dei Ricercatori 
     --che hanno almeno un impegno per didattica?
-SELECT P.nome, P.cognome, P.posizione
+SELECT DISTINCT P.nome, P.cognome, P.posizione
 FROM persona P
-JOIN attivitaNonProgettuale ANP ON P.id = ANP.persona
+JOIN attivitaNonProgettuale ANP 
+    ON P.id = ANP.persona
 WHERE P.posizione = 'Ricercatore'
     AND ANP.tipo = 'Didattica';
 
@@ -56,50 +85,97 @@ WHERE P.posizione = 'Ricercatore'
     --che hanno più di un impegno per didattica?
 SELECT P.nome, P.cognome, P.posizione
 FROM persona P
-JOIN attivitaNonProgettuale ANP ON P.id = ANP.persona
+JOIN attivitaNonProgettuale ANP 
+    ON P.id = ANP.persona
 WHERE P.posizione = 'Ricercatore'
     AND ANP.tipo = 'Didattica'
 GROUP BY P.nome, P.cognome, P.posizione
 HAVING COUNT(ANP.id) > 1;
+--VERSIONE 2
+--FROM persona PA,
+--  attivitanonprogettuale ANP
+--  attivitanonprogettuale ANP1
+--  persona P1
+--WHERE PA.id = ANP.persona 
+--  AND ANP.tipo = 'Didattica'
+--  AND ANP1.tipo = 'Didattica'
+--  AND PP.posizione = 'Ricercatore'
+--  AND ANP.id <>ANP1.id
+--  AND ANP.persona = ANP1.persona
+
 
 --8. Quali sono il nome e il cognome degli strutturati 
     --che nello stesso giorno hanno sia attività progettuali che attività non progettuali?
-SELECT P.nome, P.cognome
+SELECT DISTINCT P.nome, P.cognome
 FROM persona P
-JOIN attivitaProgetto AP ON P.id = AP.persona
-JOIN attivitaNonProgettuale ANP ON P.id = ANP.persona
+JOIN attivitaProgetto AP 
+    ON P.id = AP.persona
+JOIN attivitaNonProgettuale ANP 
+    ON P.id = ANP.persona
 WHERE AP.giorno = ANP.giorno;
 
 --9. Quali sono il nome e il cognome degli strutturati 
     --che nello stesso giorno hanno sia attività progettuali che attività non progettuali? 
     --Si richiede anche di proiettare il giorno, il nome del progetto, 
     --il tipo di attività non progettuali e la durata in ore di entrambe le attività.
-SELECT P.nome, P.cognome, AP.giorno, PR.nome AS nome_progetto, 
+SELECT DISTINCT P.nome, P.cognome, AP.giorno, PR.nome AS nome_progetto, 
        ANP.tipo AS tipo_attivita_non_progettuale, AP.oreDurata AS ore_attivita_progetto, 
        ANP.oreDurata AS ore_attivita_non_progetto
 FROM persona P
-JOIN attivitaProgetto AP ON P.id = AP.persona
-JOIN progetto PR ON AP.progetto = PR.id
-JOIN attivitaNonProgettuale ANP ON P.id = ANP.persona
+JOIN attivitaProgetto AP 
+    ON P.id = AP.persona
+JOIN progetto PR 
+    ON AP.progetto = PR.id
+JOIN attivitaNonProgettuale ANP 
+    ON P.id = ANP.persona
 WHERE AP.giorno = ANP.giorno;
- 
+--VERSIONE 2
+--SELECT  DISTINCT
+--  S.nome, S.cognome, AP.giorno
+--  P.nome AS progetto
+--  ANP.tipo AS TipoAttNonProgettuale,
+--  AP.durata AS durataAttProg
+--  ANO.durata AS durataAttNonProg
+--FROM attivitaprogetto AP  
+--  JOIN attivitaNonProgettuale ANP
+--      ON AP.persona = ANP.persona
+--  JOIN persona S
+--      ON S.id = AP.persona
+--  JOIN progetto P
+--      ON AP.progetto = P.id
+--  WHERE AP.giorno = ANP.giorno;
+      
 --10. Quali sono il nome e il cognome degli strutturati che nello stesso giorno 
     --sono assenti e hanno attività progettuali?
-SELECT P.nome, P.cognome
+SELECT DISTINCT P.nome, P.cognome
 FROM persona P
-JOIN attivitaProgetto AP ON P.id = AP.persona
-JOIN assenza A ON P.id = A.persona
+    JOIN attivitaProgetto AP 
+        ON P.id = AP.persona
+    JOIN assenza A 
+        ON P.id = A.persona
 WHERE AP.giorno = A.giorno;
+--VERSIONE 2
+--SELECT DISTINCT PE.nome, PE.cognome
+--FROM attivitaProgetto AS A1
+--  persona AS PE
+--  assenza AS A2
+--WHERE
+--  PE.id = A2.persona
+--  AND PE.id = A1.persona
+--  AND A1.giorno = A2.giorno;
 
 --11. Quali sono il nome e il cognome degli strutturati che nello stesso giorno 
     --sono assenti e hanno attività progettuali? Si richiede anche di proiettare il giorno, 
     --il nome del progetto, la causa di assenza e la durata in ore della attività progettuale.
-SELECT P.nome, P.cognome, A.giorno, PR.nome AS nome_progetto, A.tipo AS causa_assenza, 
+SELECT DISTINCT P.nome, P.cognome, A.giorno, PR.nome AS nome_progetto, A.tipo AS causa_assenza, 
        AP.oreDurata AS ore_attivita_progetto
 FROM persona P
-JOIN attivitaProgetto AP ON P.id = AP.persona
-JOIN progetto PR ON AP.progetto = PR.id
-JOIN assenza A ON P.id = A.persona
+    JOIN attivitaProgetto AP 
+        ON P.id = AP.persona
+    JOIN progetto PR 
+        ON AP.progetto = PR.id
+    JOIN assenza A 
+        ON P.id = A.persona
 WHERE AP.giorno = A.giorno;
 
 --12. Quali sono i WP che hanno lo stesso nome, ma appartengono a progetti diversi?
@@ -107,4 +183,10 @@ SELECT WP.nome, COUNT(DISTINCT WP.progetto) AS progetti_diversi
 FROM wp WP
 GROUP BY WP.nome
 HAVING COUNT(DISTINCT WP.progetto) > 1;
+--VERSIONE2
+--SELECT DISTINCT WP.nome
+--FROM wp WP1, wp WP2
+--WHERE WP1.nome = WP2.nome
+--  AND WP1.progetto <> WP2.progetto;
+--
 
