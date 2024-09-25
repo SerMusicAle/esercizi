f5b777f2f88dfcc3e09a7e2ac386dfd3df36bf56
SerMusicAle <a.sereni@live.it>
2024-09-25 19:08:24 +0200

    update

---
+++ 000_GIT/clone repository.py
@@ -0,0 +1,10 @@
+#dati
+git config --global user.name "IlTuoNomeUtente"
+git config --global user.email "iltuo@email.com"
+
+#verifiche
+git config --global --list
+git status
+
+
+git clone https://github.com/username/nomecartella
