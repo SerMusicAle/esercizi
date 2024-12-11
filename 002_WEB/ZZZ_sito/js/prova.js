2ba057344883be0bcdd25a9477cc17c95c68b2b6
Alessandro Sereni <167187453+SerMusicAle@users.noreply.github.com>
2024-12-11 09:23:45 +0100

    Delete 002_WEB directory

--- 002_WEB/ZZZ_sito/js/prova.js
+++
@@ -1,9 +0,0 @@
-function Somma(n1, n2) {
-    return n1 + n2;
-}
-console.log("inizio programma");
-var num1 = 10;
-var num2 = 20;
-var somma;
-somma = Somma(num1, num2);
-console.log("La somma di " + num1 + " + " + num2 + "vale" + somma);
